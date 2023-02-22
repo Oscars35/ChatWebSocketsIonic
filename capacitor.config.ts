7a0aa757486d71867aebe01496972ae2b325e7a5
Oscars35 <oscar13785@gmail.com>
2023-02-22 10:49:55 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ChatWebSockets',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
