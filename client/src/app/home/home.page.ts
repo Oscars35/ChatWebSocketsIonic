3d5465f7cccd19eb5226b069dfd0519ae91de0ec
Oscars35 <oscar13785@gmail.com>
2023-02-23 10:21:54 +0100

    Chat works better, need to refactor and extract classes

--- client/src/app/home/home.page.ts
+++ client/src/app/home/home.page.ts
@@ -13,13 +13,14 @@ export class HomePage {
   message: string;
   socket: WebSocket;
   user: string | null;
+  myName: string | null;
 
   constructor() {
     this.socket = new SockJS('http://localhost:8080/chat');
     this.socket.onopen = () => {
-      var name: string | null = "";
-      while (name == "") name = prompt("Enter your name");
-      this.sendMessage("join", name);
+      this.myName = "";
+      while (this.myName == "") this.myName = prompt("Enter your name");
+      this.sendMessage("join", this.myName);
       setInterval(() => { this.socket.send(JSON.stringify({type: "update", data: name})); }, 3000);
     }
 
@@ -27,7 +28,7 @@ export class HomePage {
       this.receievMsg(JSON.parse(msg.data)) 
     }
 
-    this.socket.onclose = () => { alert("Server Disconnect You"); }
+    this.socket.onclose = () => { }
   }
 
   sendMessage(type: string, data: any) {
@@ -40,8 +41,7 @@ export class HomePage {
 
   private receievMsg(msg: any) {
     if (msg.msgType == "say") {
-      console.log("SAAAY");
-      $(".chat-messages").append("<p>"  + this.user + ": "  +msg.data+"</p>");
+      $(".chat-messages").append("<p>"  + msg.user.name + ": "  +msg.data+"</p>");
     }
     else if (msg.msgType == "join") {
       this.addUser(msg.data);
@@ -51,6 +51,8 @@ export class HomePage {
     }
     else if (msg.msgType == "left") {
       $("#user-"+msg.data.id).remove();
+      $(".chat-messages").append("<p>" + msg.data.name + ' Left chat' + "</p>");
+      $("#user-"+msg.data.id+"leaderboard").remove();
     }
     else if (msg.msgType == "update") {
       this.updateLeaderboard(msg.data);
@@ -58,8 +60,8 @@ export class HomePage {
   }
 
   addUser(user: any) {
-    console.log("ADDUSER");
     $("#user-list").append("<li id='user-"+user.id+"'>"+user.name+"</li>");
+    $(".chat-messages").append("<p>" + user.name + ' joined chat' + "</p>");
   }
 
   updateLeaderboard(user: any) {
@@ -71,6 +73,14 @@ export class HomePage {
     }
     else {
         $("#leaderboard-list").append("<li id='user-"+user.id+"leaderboard'>"+user.name+": "+user.score+"</li>");
+    }
+  }
+
+  leaveChat() {
+    if(this.socket.readyState === WebSocket.OPEN) {
+      this.socket.send(JSON.stringify({type: "left", data: this.myName}));
+      $(".chat-messages").append("<p>You left chat</p>");
+      this.socket.close();
     }
   }
 
